0274e7f267401b51113c264eeb5d11070345514f
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-06-16 23:09:51 +0530

    add filter component

--- frontend/MERN_Ecommerce_App/src/App.jsx
+++ frontend/MERN_Ecommerce_App/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from '../components/Navbar'
+import Filter from '../components/Filter'
 function App() {
 
   return (
     <>
       <Navbar />
+      <Filter />
     </>
   )
 }
