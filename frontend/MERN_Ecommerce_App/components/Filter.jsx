9617775ee1f69ef47237c2d6af7598d608f9dd5b
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-06-19 23:07:15 +0530

    added filter options

--- frontend/MERN_Ecommerce_App/components/Filter.jsx
+++ frontend/MERN_Ecommerce_App/components/Filter.jsx
@@ -1,6 +1,13 @@
 const Filter = () => {
   return (
-    <div className="filter">Filter</div>
+    <div className="filter">
+      <ul>
+        <li>ALL</li>
+        <li>Electronics</li>
+        <li>Food</li>
+        <li>Gym</li>
+      </ul>
+    </div>
   )
 }
 
