11c534e417967d584a93b1a85dfcbf8e47354025
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-07-03 02:08:39 +0530

    add product controller

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,3 +1,20 @@
+const PRODUCT = require('../models/product')
+
 const getAllProducts = async(req,res) =>{
+     let data = await PRODUCT.find({})
+     res.status(200).json({total:data.length , data : data})
+    // res.send('hello')
+}
+const getSpecificProduct = async(req,res) => {
     
 }
+const createProduct = async( req,res) => {
+
+}
+const updateProduct = async(req,res) =>{
+
+}
+const deleteProduct = async(req,res) =>{
+    
+}
+module.exports = getAllProducts
