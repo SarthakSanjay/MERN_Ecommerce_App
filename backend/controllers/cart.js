4acb6c902afdfd972fb6cbba7cc973ee8dd7ecfc
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-07-03 02:05:01 +0530

    add cart controller

---
+++ backend/controllers/cart.js
@@ -0,0 +1,21 @@
+
+const cartDetails = async() =>{
+
+}
+const addProductToCart = async(req,res) =>{
+
+}
+const removeProductFromCart = async(req,res) =>{
+
+}
+
+const checkout = async(req,res) =>{
+
+}
+
+module.exports = {
+    cartDetails,
+    addProductToCart,
+    removeProductFromCart,
+    checkout
+}
