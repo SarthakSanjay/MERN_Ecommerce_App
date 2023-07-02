2ead09e49b1c7875b7accdbf70f9eb8b56f25a64
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-07-03 02:05:31 +0530

    add order controller

---
+++ backend/controllers/order.js
@@ -0,0 +1,17 @@
+const getAllOrder = async(req,res) =>{
+
+}
+const getSpecificOrder = async(req,res) =>{
+
+}
+const createOrder = async(req,res) =>{
+
+}
+const updateOrder = async(req,res) =>{
+
+}
+const removeOrder = async(req,res) =>{
+
+}
+
+module.exports = {getAllOrder,getSpecificOrder,createOrder,updateOrder,removeOrder}
