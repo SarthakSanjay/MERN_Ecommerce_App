0ace088154f0bb304e8fdf9d16f17899d8cefae2
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-07-04 00:48:14 +0530

    add controller function

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -6,10 +6,30 @@ const getAllProducts = async(req,res) =>{
     // res.send('hello')
 }
 const getSpecificProduct = async(req,res) => {
-    
-}
+    const productId = req.params.productId;
+  
+    // Query the database to retrieve the product
+    const product = PRODUCT.findById(productId);
+  
+    if (!product) {
+      // Handle case where product is not found
+      return res.status(404).json({ error: 'Product not found' });
+    }
+  
+    // Prepare and return the response
+    res.json(product);
+  }
 const createProduct = async( req,res) => {
-
+    const product = PRODUCT.create({
+        productName: productName ,
+        price: price,
+        companyName: companyName,
+        rating: rating
+    })
+    if(!product){
+        return res.status(200).json({msg:"product inserted successfully"})
+    }
+    res.json(product)
 }
 const updateProduct = async(req,res) =>{
 
